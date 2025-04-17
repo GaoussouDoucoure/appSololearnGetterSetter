b339e56293dff09709b469f1a836a5ef44421844
Gaoussou Doucoure <gaousd4@gmail.com>
2025-04-16 19:17:52 -0700

    update

--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
+class Student {
+    public String name;
+    private int age;
 
-public class Student {
+    public int getAge(){
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+        if(age < 0){
+            System.out.println("Invalid age");
+            this.age = 0;
+        }
+    }
 }
